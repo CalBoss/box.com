712b8f0ce477781d0d5cc35ef31ca6f178fd686d
Hugo Mejia <hugomejia74@gmail.com>
2016-01-21 19:50:19 -0800

    Renaming original uploadfile.py into updatefile.py | Modding original uploadfile.py to not need a file ID to allow a completely new file to be loaded into folder.

--- apicalls/updatefile.py
+++ apicalls/updatefile.py
@@ -8,11 +8,12 @@ import json
 #$ python ~/path_to_file.file.txt 1234567 7654321
 
 
-if len(sys.argv) != 3: #first element is script name. 
-    sys.exit("Aborting. You must provide in this order: filename path and folder id")
+if len(sys.argv) != 4: #first element is script name. 
+    sys.exit("Aborting. You must provide in this order: filename path and folder id and existing file id (so it can be replaced if already exists)")
 else:
     fname = sys.argv[1]
     fid = sys.argv[2]
+    exist_id = sys.argv[3]
 
 data = {"parent_id": fid}
 files = {"filename": open(fname,'rb') } #fid is name of folder in box you are uploading to. fname is name of file to upload (given as argument). 
@@ -21,9 +22,9 @@ access_token = json.loads(open('../token/access_token.json').read())['access_tok
 
 header = {'Authorization':'Bearer %s' % access_token}
 
-upload_url = "https://upload.box.com/api/2.0/files/content"
+upload_url = "https://upload.box.com/api/2.0/files/%s/content"
 
-resp = requests.post(upload_url, headers=header,data=data,files=files)
+resp = requests.post(upload_url % exist_id, headers=header,data=data,files=files)
 print("api call, response code: %s" % resp.status_code)
 print(resp.text)
 
